8dfe7b5652231cf3df37e42341d368dbdc969d1f
Julian George <jules104@gmail.com>
2020-03-10 16:33:32 +0000

    Updated to take in breed with jq

--- src/app.js
+++ src/app.js
@@ -1,25 +1,15 @@
-function ajax_get(url, callback) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      console.log('responseText:' + xmlhttp.responseText);
-      try {
-        var data = JSON.parse(xmlhttp.responseText);
-      } catch (err) {
-        console.log(err.message + ' in ' + xmlhttp.responseText);
-        return;
-      }
-      callback(data);
-    }
-  };
+var apikey = config.API_KEY;
 
-  xmlhttp.open('GET', url, true);
-  xmlhttp.send();
-}
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: 'https://api.thecatapi.com/v1/breeds?attach_breed=0',
+  method: 'GET',
+  headers: {
+    'x-api-key': '${apikey}'
+  }
+};
 
-ajax_get('https://api.thecatapi.com/v1/breeds?attach_breed=0', function(data) {
-  var selection = Math.floor(Math.random() * 50);
-  document.getElementById('breed').innerHTML = data[selection]['name'];
-  document.getElementById('wikipedia_url').innerHTML =
-    data[selection]['wikipedia_url'];
+$.ajax(settings).done(function(response) {
+  console.log(response);
 });
